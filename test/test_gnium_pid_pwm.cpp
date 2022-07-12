d4be86b37026abca191d562f41eb74c3a1f25019
wis <is.jaras@gmail.com>
2022-07-12 19:06:27 -0400

    improve gnium creation testing

--- test/test_gnium_pid_pwm.cpp
+++ test/test_gnium_pid_pwm.cpp
@@ -24,6 +24,9 @@ void test_example(void) {
 void test_gnium_creation_succeed(void) {
     // TODO improve testing scheme
     gnium::Gnium gnium = gnium::Gnium(0, 0, 0);
+    TEST_ASSERT_EQUAL(gnium.Mode, 0);
+    TEST_ASSERT_EQUAL(gnium.Type, 0);
+    TEST_ASSERT_EQUAL(gnium.Lap_n, 0);
     gnium::Gnium gnium2 = gnium::Gnium(1, 0, 0);
     gnium::Gnium gnium3 = gnium::Gnium(0, 1, 0);
     gnium::Gnium gnium4 = gnium::Gnium(1, 1, 0);
