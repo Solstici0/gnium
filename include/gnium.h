6ed2349dce9b138cfc2c2249b868957de992507f
Ismael Jaras <is.jaras@gmail.com>
2022-08-31 21:27:32 -0400

    include pid logic in gnium

--- include/gnium.h
+++ include/gnium.h
@@ -19,11 +19,15 @@ class gnium::Gnium {
         // Constructor
         //TODO (wis) in principle, there should be 2 types of gnium
         // with 5 wheels, and the standar 3 wheels version
-        Gnium(int mode, int type = 0, int lap_n = 0, int train_pwm = 10) {
+        Gnium(int mode, int type = 0, int lap_n = 0,
+              int train_pwm = 10, pid::Pid pid = pid::Pid()) {
             Mode = mode;  // 0 -> testing, 1-> compite
             Type = type;  // 0 -> 5-wheels, 1 -> classic 
             Lap_n = lap_n;  // lap number counter
             Train_pwm = train_pwm;  // pwm train velocity
+            // TODO (wis) inject pid object as attribute..
+            // check if this could be improve
+            Pid = pid; // PID controller
         }
 
     public:
@@ -33,9 +37,10 @@ class gnium::Gnium {
         int Train_pwm;  // pwm train velocity
         bool Start_detected = false;  // bool start line detected
         bool End_detected = false;  // bool end line detected
+        pid::Pid Pid;  //PID controller
 
     void reset_start_and_end() {
-        /*!< @fn reset_start_and_end
+        /*! @fn reset_start_and_end
           * reset Start and End detected Booleans
           */
           gnium::Gnium::Start_detected = false;
@@ -67,37 +72,39 @@ class gnium::Gnium {
         return fail;
     }
 
-    int train(int train_pwm = -1) {
+    int train(int train_pwm = -1,
+              pid::Pid pid = pid::Pid()) {
         /*! @fn train protocol for first lap
          * @param train_pwm forward velocity for training
-         * TODO @param pid should be a vector of 3 elements
+         * @param pid object
          * TODO (wis) training protocol for 1st lap
          * when running this function we should save the lap database.
          * Where this should be saved? lap class (struct)?
          * gnium attribute?
          */
 
-         // use default Train_pwm velocity if its not defined
+         // use default Train_pwm velocity and PID values
          if (train_pwm == -1) {
              train_pwm = gnium::Gnium::Train_pwm;
+             pid = gnium::Gnium::Pid;
          }
 
          // run until start line is detected
          while (!gnium::Gnium::Start_detected) {
              gnium::Gnium::Start_detected =
-                 gnium::Gnium::follow_trace(train_pwm);
+                 gnium::Gnium::follow_trace(train_pwm, pid);
          }
 
          // run until end line is detected
          while (gnium::Gnium::Start_detected and
              !gnium::Gnium::End_detected) {
              gnium::Gnium::End_detected =
-                 gnium::Gnium::follow_trace(train_pwm);
+                 gnium::Gnium::follow_trace(train_pwm, pid);
          }
 
          // end detected routine
          if (gnium::Gnium::End_detected) {
-             gnium::Gnium::follow_trace(train_pwm);
+             gnium::Gnium::follow_trace(train_pwm, pid);
              // here we can stop for some time
              // and then exit function
              return 0;
