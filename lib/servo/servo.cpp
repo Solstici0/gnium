21eb13b89be6183aac0153f0ec66ef52535bf003
Ismael Jaras <is.jaras@gmail.com>
2022-10-12 22:19:36 -0300

    include some todos

--- lib/servo/servo.cpp
+++ lib/servo/servo.cpp
@@ -1,6 +1,11 @@
 #include "servo.h"
 
+// TODO (wis) check servo's libs for arduino
 namespace servo{
   void setup(){
+  }
+
+  void set_angle(float angle_correction){
+    // TODO (wis) send information to servo
   }
 }
